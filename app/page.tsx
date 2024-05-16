b3fe33085ec3ba6917c08c2d6210e7c4330e8431
sandevadiga <santhoshrdevadiga@gmail.com>
2024-05-16 13:29:31 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,66 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react';
 
-export default function Home() {
+function Page() {
+  const [price, setGoldPrice] = useState<string>("0.00");
+  const getGoldPrice = async () => {
+    try {
+      const response = await fetch('https://www.bajajfinserv.in/gold-rate-today-in-kalyan', { mode: 'no-cors' });
+      const html = await response.text();
+      console.log(response)
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+      const gold22kElement = Array.from(doc.querySelectorAll("b")).find(el => el.textContent?.includes('22K Gold/10 gm'));
+      if (gold22kElement) {
+        const goldPriceElement = gold22kElement.nextElementSibling?.querySelector("h3");
+        if (goldPriceElement) {
+          const goldPrice = goldPriceElement.textContent?.trim() || '';
+          setGoldPrice(goldPrice);
+        } else {
+          setGoldPrice('Price not found');
+        }
+      } else {
+        setGoldPrice('Gold rate not found');
+      }
+    } catch (error) {
+      console.error('Error fetching gold price:', error);
+      setGoldPrice('Error fetching gold price');
+    }
+  }
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='bg-gray-100'>
+      <div className='w-full  text-center h-30 bg-amber-400 p-4 text-2xl'>
+        <h1>Daily Gold Rates</h1>
+      </div>
+      <div className='flex flex-col h-screen  bg-red-300'>
+        {/* one */}
+        <div className='flex justify-around items-center  bg-yellow-800 text-white p-2 border-2 border-neutral-600'>
+          <span>Joy Alukas</span>
+          <button className='p-3 rounded bg-green-500' onClick={() => setGoldPrice("100")}> Get Price</button>
+          <h1 className='bg-yellow-500 p-2 rounded'>Gold Rate: {price}</h1>
+          <h1 className='bg-zinc-300 p-2 rounded text-black'> Sliver Rate: {price}</h1>
+          <button className='p-3 rounded bg-green-400' onClick={() => alert("this website has under daily gold price ")}> why No</button>
+        </div>
+        {/* two */}
+        <div className='flex justify-around items-center  bg-yellow-800 text-white p-2  border-2 border-neutral-600'>
+          <span>Jos Alukas</span>
+          <button className='p-3 rounded bg-green-500' onClick={() => setGoldPrice("100")}> Get Price</button>
+          <h1 className='bg-yellow-500 p-2 rounded'>Gold Rate: {price}</h1>
+          <h1 className='bg-zinc-300 p-2 rounded text-black'> Sliver Rate: {price}</h1>
+          <button className='p-3 rounded bg-green-400' onClick={() => alert("this website has under daily gold price https://www.josalukkasonline.com/compare")}> why No</button>
+        </div>
+        {/* three*/}
+        <div className='flex justify-around items-center  bg-yellow-800 text-white p-2  border-2 border-neutral-600'>
+          <span>kalyan jewellers</span>
+          <button className='p-3 rounded bg-green-500' onClick={getGoldPrice}> Get Price</button>
+          <h1 className='bg-yellow-500 p-2 rounded'>Gold Rate: {price}</h1>
+          <h1 className='bg-zinc-300 p-2 rounded text-black'> Sliver Rate: {price}</h1>
+          <button className='p-3 rounded bg-green-400' onClick={() => alert("this website has under daily gold price https://www.josalukkasonline.com/compare")}> why No</button>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
+
+export default Page;
