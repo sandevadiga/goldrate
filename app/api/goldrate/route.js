8d8eea6668f63865323d15306fafe5b1bf682b78
sandevadiga <santhoshrdevadiga@gmail.com>
2024-05-16 14:39:32 +0530

    d

--- app/api/goldrate/route.js
+++ app/api/goldrate/route.js
@@ -1,13 +1,11 @@
-
-
-export const GET = async(request)=>{
- try{
-    const response = await fetch('https://www.bajajfinserv.in/gold-rate-today-in-kalyan')
-    return new Response(response.body, { status: 200 })
- }
- catch(error){
-    return new Response("Failed to grt price", { status: 500 })
- }
-    // const url = request.headers.get('targetUrl')
+export const GET = async(request) => {
+    try {
+      const response = await fetch('https://www.bajajfinserv.in/gold-rate-today-in-kalyan');
+      const data = await response.json();
+      return new Response(JSON.stringify(data), { status: 200 });
+    } catch(error) {
+      return new Response("Failed to get price", { status: 500 });
+    }
+    // const url = request.headers.get('targetUrl');
+  }
   
-}
